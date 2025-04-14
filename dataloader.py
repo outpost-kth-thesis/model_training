3087c6ec6bfa6202f0c17aca60ce46c980a335f4
ohshitnotgood <praanto@icloud.com>
2025-04-14 15:36:53 +0200

    removed train-val split

--- dataloader.py
+++ dataloader.py
@@ -13,17 +13,17 @@ class OPKDataset(Dataset):
         self.root_dir = root_dir
 
         self._walk_root_directory()
-        self.train_files = self.all_files[:int(0.8*len(self.all_files))]
-        self.val_files = self.all_files[-int(0.2*len(self.all_files)):]
+        # self.train_files = self.all_files[:int(0.8*len(self.all_files))]
+        # self.val_files = self.all_files[-int(0.2*len(self.all_files)):]
         self.split = split
         
         super().__init__()
 
     def __len__(self):
-        return len(self.train_files) if self.split == "train" else len(self.val_files)
+        return len(self.all_files)
 
     def __getitem__(self, index):
-        minified_filepath = self.train_files[index] if self.split == 'train' else self.val_files[index]
+        minified_filepath = self.all_files[index]
         original_filepath = minified_filepath.replace("_terser.min.js", ".js") if "_terser" in minified_filepath else minified_filepath.replace("_google.min.js", ".js")
         minified_file_content = open(file=minified_filepath).read()
         original_file_content = open(file=original_filepath).read()
@@ -35,8 +35,5 @@ class OPKDataset(Dataset):
                 if ".min.js" in file:
                     self.all_files.append(os.path.join(root, file))
 
-    def _walk_plaintext_directory(self):
-        pass
-
-dt = OPKDataset(split="train", root_dir="/home/jovyan/outpost-thesis/datasets/minified")
+dt = OPKDataset(root_dir="/home/jovyan/outpost-thesis/datasets/minified")
 print(len(dt))
