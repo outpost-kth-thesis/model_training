0232af1ca9d2b8eca2f14491bf6aa10e5e855ad9
ohshitnotgood <praanto@icloud.com>
2025-04-14 14:38:13 +0200

    getItem now returns minified and original file

--- dataloader.py
+++ dataloader.py
@@ -1,12 +1,12 @@
 from torch.utils.data import DataLoader, Dataset
 import os
 
-class MinifierDataset(Dataset):
+class OPKDataset(Dataset):
     all_files = []
     train_files = []
     val_files = []
     split = 'train'
-    root_dir = 'data_ceche/'
+    root_dir = 'minifier/'
     
 
     def __init__(self, split='train', root_dir='data_cache/'):
@@ -23,19 +23,22 @@ class MinifierDataset(Dataset):
         return len(self.all_files)
 
     def __getitem__(self, index):
-        file = self.train_files[index] if self.split == 'train' else self.val_files[index]
-        file_content = open(self.all_files[index]).read()
-        return file_content
+        minified_filepath = self.train_files[index] if self.split == 'train' else self.val_files[index]
+        original_filepath = minified_filepath.replace("_terser.min.js", ".js") if "_terser" in minified_filepath else minified_filepath.replace("_google.min.js", ".js")
+        minified_file_content = open(file=minified_filepath).read()
+        original_file_content = open(file=original_filepath).read()
+        return minified_file_content, original_file_content
     
     def _walk_root_directory(self):
         for root, _, files in os.walk(self.root_dir):
             for file in files:
-                self.all_files.append(os.path.join(root, file))
+                if ".min.js" in file:
+                    self.all_files.append(os.path.join(root, file))
 
     def _walk_plaintext_directory(self):
         pass
 
-dt = MinifierDataset("/home/praanto/Projects/outpost-thesis/obf-data/data_extracted")
+dt = OPKDataset("/home/praanto/Projects/outpost-thesis/obf-data/data_extracted")
 
 r = dt.__getitem__(3)
 print(r)
