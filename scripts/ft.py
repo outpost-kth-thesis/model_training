03a4cf401e50024aae9df8dc5e159942d1e710c9
ohshitnotgood <praanto@icloud.com>
2025-04-20 17:56:14 +0200

    fixed model

--- scripts/ft.py
+++ scripts/ft.py
@@ -11,22 +11,10 @@ load_dotenv()
 
 class CausalLM(pl.LightningModule):
     def __init__(self, lr=5e-5):
+        super().__init__()
         self.save_hyperparameters()
         self.model_name = os.getenv("MODEL_NAME")
 
-    def forward(self, input_ids, attention_mask, labels):
-        self.model(input_ids, attention_mask, labels)
-
-    def training_step(self, batch, batch_idx):
-        output = self(**batch)
-        loss = output.loss
-        self.log("train_loss", loss)
-        return loss
-    
-    def configure_optimizers(self):
-        return torch.optim.AdamW(self.parameters(), lr=self.hparams.lr)
-
-    def configure_model(self):
         quantization_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.bfloat16,
@@ -37,6 +25,34 @@ class CausalLM(pl.LightningModule):
         self.model = AutoModelForCausalLM.from_pretrained(self.model_name, quantization_config=quantization_config)
         self.model.resize_token_embeddings(len(get_tokenizer()))
         self.model.config.pad_token = pad_token
+
+    def forward(self, input_ids, attention_mask, labels):        
+        return self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            labels=labels
+        )
+
+    def training_step(self, batch, batch_idx):
+        output = self(**batch)
+        loss = output.loss
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+    
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self.hparams.lr)
+
+    # def configure_model(self):
+    #     quantization_config = BitsAndBytesConfig(
+    #         load_in_4bit=True,
+    #         bnb_4bit_compute_dtype=torch.bfloat16,
+    #         bnb_4bit_quant_type="nf4",
+    #         bnb_4bit_use_double_quant=True,
+    #     )
+
+    #     self.model = AutoModelForCausalLM.from_pretrained(self.model_name, quantization_config=quantization_config)
+    #     self.model.resize_token_embeddings(len(get_tokenizer()))
+    #     self.model.config.pad_token = pad_token
 
 
 
